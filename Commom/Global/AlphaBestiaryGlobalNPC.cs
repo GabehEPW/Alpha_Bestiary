af27319b35a57436ef2b491769b6d6059d1b0df4
GabehEPW <gabriel.epw@outlook.com>
2026-03-03 12:07:32 -0300

    GlobalNPC atualizado para a Wand

--- Commom/Global/AlphaBestiaryGlobalNPC.cs
+++ Commom/Global/AlphaBestiaryGlobalNPC.cs
@@ -11,8 +11,7 @@ namespace AlphaBestiary.Common.Global
         private int lastHitItemType = 0;
         private int lastHitPlayerWhoAmI = -1;
 
-        // Assinatura correta na sua versão:
-        // OnHitByItem(NPC npc, Player player, Item item, NPC.HitInfo hit, int damageDone)
+        // 1) Hit direto com item (espada, etc.)
         public override void OnHitByItem(NPC npc, Player player, Item item, NPC.HitInfo hit, int damageDone)
         {
             base.OnHitByItem(npc, player, item, hit, damageDone);
@@ -25,6 +24,30 @@ namespace AlphaBestiary.Common.Global
             }
         }
 
+        // 2) Hit por projétil (cajado, arco, armas de tiro)
+        public override void OnHitByProjectile(NPC npc, Projectile projectile, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitByProjectile(npc, projectile, hit, damageDone);
+
+            // Só projéteis que têm dono válido
+            if (projectile.owner < 0 || projectile.owner >= 255)
+                return;
+
+            Player player = Main.player[projectile.owner];
+
+            // Item que o player está segurando quando atirou (aproximação boa pra arma mágica / ranged)
+            Item item = player.HeldItem;
+
+            if (item != null && !item.IsAir &&
+                item.ModItem != null &&
+                item.ModItem.Mod == ModContent.GetInstance<AlphaBestiary>())
+            {
+                lastHitItemType = item.type;
+                lastHitPlayerWhoAmI = player.whoAmI;
+            }
+        }
+
+        // 3) Quando o NPC morre, registrar a kill para a arma certa
         public override void OnKill(NPC npc)
         {
             base.OnKill(npc);
